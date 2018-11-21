e7b400883e9bf2470b3f4020e4e3172cb50091c1
Shin Yamamoto <shin@scenee.com>
2018-11-21 09:11:14 +0900

    Add floatingPanelShouldBeginDragging(_:) delegate method
    * Update doc comments

--- Framework/Sources/FloatingPanel.swift
+++ Framework/Sources/FloatingPanel.swift
@@ -280,6 +280,11 @@ class FloatingPanel: NSObject, UIGestureRecognizerDelegate, UIScrollViewDelegate
                 self.animator = nil
             }
 
+            if interactionInProgress == false,
+                viewcontroller.delegate?.floatingPanelShouldBeginDragging(viewcontroller) == false {
+                return
+            }
+
             switch panGesture.state {
             case .began:
                 panningBegan()
